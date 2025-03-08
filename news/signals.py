3a4cb6489a6e975d6df7383d18db62890a412503
uuuggglllyyy <great.egor7288@gmail.com>
2025-03-08 15:09:37 +0500

    authors

---
+++ news/signals.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Author
+
+
+@receiver(post_save, sender=User)
+def create_user_author(sender, instance, created, **kwargs):
+    if created:
+        Author.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_author(sender, instance, **kwargs):
+    instance.author.save()
